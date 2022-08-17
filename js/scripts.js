7526d5bc172ec6fdfedf81f152fcbeda6bb4ef98
cas-sav <94647032+cas-sav@users.noreply.github.com>
2022-08-16 23:23:25 -0400

    Fixed typo & added loop/conditional

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,27 @@
 let pokemonList = [
 	{	name: 'Vulpix',
 		height: 0.6,
-		types: ['Fire']}
+		types: ['Fire']
+	},
 
 	{	name: 'Moltres',
 		height: 2,
-		types: ['Fire', 'Flying']}
+		types: ['Fire', 'Flying']
+	},
 
-	{	name 'Doduo',
+	{	name: 'Doduo',
 		height: 1.4,
-		types: ['Flying', 'Fire']}
+		types: ['Flying', 'Fire']
+	}
 ];
+
+
+for (let i = 0; i < pokemonList.length; i++) {
+   
+	if (pokemonList[i].height > 1.5) {
+		document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") " + "- Wow, that's big!");
+	} else {
+		document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") ");
+	}
+
+}

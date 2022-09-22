37edcc7ede486a71fd399b053e0c6676de1ee0f4
cas-sav <94647032+cas-sav@users.noreply.github.com>
2022-09-22 10:38:15 -0400

    Added an IIFE and updated the forEach

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,39 @@
-let pokemonList = [
-	{	name: 'Vulpix',
-		height: 0.6,
-		types: ['Fire']
-	},
+let pokemonRepository = (function () {	
 
-	{	name: 'Moltres',
-		height: 2,
-		types: ['Fire', 'Flying']
-	},
+	let pokemonList = [
+		{	name: 'Vulpix',
+			height: 0.6,
+			types: ['Fire']
+		},
 
-	{	name: 'Doduo',
-		height: 1.4,
-		types: ['Flying', 'Fire']
+		{	name: 'Moltres',
+			height: 2,
+			types: ['Fire', 'Flying']
+		},
+
+		{	name: 'Doduo',
+			height: 1.4,
+			types: ['Flying', 'Fire']
+		}
+	];
+
+	function add(item) {
+		pokemonList.push(item);
 	}
-];
+
+	function getAll() {
+		return pokemonList;
+	}
+
+	return {
+		add: add,
+		getAll: getAll
+	}
+
+})() //end of repository
 
 
-pokemonList.forEach(function(pokemon) {
+pokemonRepository.getAll().forEach(function(pokemon) {
    
 	if (pokemon.height > 1.5) {
 		document.write("<p>" + pokemon.name + " (height: " + pokemon.height + ") " + "- Wow, that's big!" + "</p>");
